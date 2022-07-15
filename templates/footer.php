6f16d8b3d34570f4affc79a72ca4abe7e9b01414
Jesus <jesus@esbrillante.mx>
2022-07-15 16:26:05 -0500

    Ajustes SEO

--- templates/footer.php
+++ templates/footer.php
@@ -3,12 +3,12 @@
 
             <div class="grid">
             <div class="col">
-                 <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Logo">
+                 <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png" alt="Dra. Megan González Soto">
             </div>
             <div class="col">
                <h2>Sígueme en: <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="Facebook"></h2>
             </div>
            </div>
             </div>
-            <p>Copyright All Rights Reserved © 2018 | <a href="https://esbrillante.mx/diseno-de-paginas-web-profesionales-empresas/" title="Diseño profesional de páginas Web"> Diseño Web por esbrillante.mx </a></p>
+            <p>Dra. Megan González Soto | Copyright All Rights Reserved © <?php echo date("Y"); ?>| <a href="https://esbrillante.mx/diseno-de-paginas-web-profesionales-empresas/" title="Diseño profesional de páginas Web"> Diseño Web por esbrillante.mx </a></p>
  </footer>
--- templates/home.php
+++ templates/home.php
@@ -2,9 +2,9 @@
     /* Template Name: Inicio */ 
     get_header();
 ?>
-  <section id="inicio_sl" class="template-section">
-       <?php echo do_shortcode( '[rev_slider alias="slider_megan"]' ); ?>
-      <!--
+<section id="inicio_sl" class="template-section">
+    <?php echo do_shortcode( '[rev_slider alias="slider_megan"]' ); ?>
+    <!--
        <div class="comienzo">
         <h2>Implante Dental</h2>
         <h3>¿Por qué esperar más?</h3>
@@ -12,33 +12,33 @@
         <a href="#" class="btn aqua">¡Me interesa!</a>
         </div>
         -->
-    </section>
-    <section id="paquetes" class="template-section padding-50">
-        <div class="contenedor">
-            <h2>PAQUETES</h2>
-             <div class="grid">
-                 <div class="col3">
-                     <div class="parte1">
+</section>
+<section id="paquetes" class="template-section padding-50">
+    <div class="contenedor">
+        <h2>PRECIOS DE PAQUETES DENTALES EN CDMX</h2>
+        <div class="grid">
+            <div class="col3">
+                <div class="parte1">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/paquete1.png" alt="paquete1">
-                    <h3>Paquete Prevención Infantil</h3>
+                    <h3>Paquete Dental Prevención Infantil</h3>
                     <h3>Incluye:</h3>
                     <ul>
                         <li>Valoración Integral.</li>
                         <li>Limpieza.</li>
                         <li>Aplicación de flúor.</li>
                         <li>2 selladores de fosetas y fisuras. <br>
- (prevención de caries).</li> 
+                            (prevención de caries).</li>
                     </ul>
                     <p class="precio margen1">$640.00</p>
-                    </div>
-                    <div class="parte2 paquete1">
-                        <p>Únicamente para uso individual. <br>
-                         Pago en una sola exhibición.</p>
-                    </div>
-                     <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
-                 </div>
-                 <div class="col3">
-                     <div class="parte1">
+                </div>
+                <div class="parte2 paquete1">
+                    <p>Únicamente para uso individual. <br>
+                        Pago en una sola exhibición.</p>
+                </div>
+                <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
+            </div>
+            <div class="col3">
+                <div class="parte1">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/paquete2.png" alt="paquete2">
                     <h3>Paquete Resinas</h3>
                     <h3>Incluye:</h3>
@@ -46,193 +46,216 @@
                         <li>Valoración.</li>
                         <li>3 resinas.</li>
                         <li>Eliminación de caries. </li>
-                        <li>Colocación y pulido de resina.</li> 
+                        <li>Colocación y pulido de resina.</li>
                     </ul>
                     <p class="precio margen2">$1,498.00</p>
-                    </div>
-                    <div class="parte2 paquete2">
-                        <p>Paquete válido durante 1 año (las resinas se pueden colocar en este lapso de tiempo). Únicamente para uso individual.
-Resinas de una sola superficie dental.
- Pago en una sola exhibición.</p>
-                    </div>
-                    <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
-                   
-                 </div>
-                 <div class="col3">
-                     <div class="parte1">
+                </div>
+                <div class="parte2 paquete2">
+                    <p>Paquete válido durante 1 año (las resinas se pueden colocar en este lapso de tiempo). Únicamente
+                        para uso individual.
+                        Resinas de una sola superficie dental.
+                        Pago en una sola exhibición.</p>
+                </div>
+                <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
+
+            </div>
+            <div class="col3">
+                <div class="parte1">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/paquete3.png" alt="paquete3">
-                    <h3>Paquete Blanqueamiento</h3>
+                    <h3>Paquete Blanqueamiento Dental</h3>
                     <h3>Incluye:</h3>
                     <ul>
                         <li>3 Limpiezas.</li>
                         <li>1 blanqueamiento. </li>
                     </ul>
                     <p class="precio margen3">$2,688.00</p>
-                    </div>
-                    <div class="parte2 paquete3">
-                        <p>Paquete válido durante 1 año.
-Únicamente para uso individual.
-El blanqueamiento es una sola aplicación.
- Pago en una sola exhibición. </p>
-                    </div>
-                     <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
-                 </div>
-             </div>
+                </div>
+                <div class="parte2 paquete3">
+                    <p>Paquete válido durante 1 año.
+                        Únicamente para uso individual.
+                        El blanqueamiento es una sola aplicación.
+                        Pago en una sola exhibición. </p>
+                </div>
+                <a href="https://dentistamegan.mx/#contacto" class="btn meinteresa">¡Me interesa!</a>
+            </div>
         </div>
-    </section>
-    <section>
-    <div id="servicios" class="template-section">
-         <h2 class="valores_titulo">SERVICIOS</h2>
-         <h3>Contamos con atención personalizada cubriendo <br>
-todas las áreas odontológicas</h3>
-         <div class="grid">
-         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/005-teeth-1.png" alt="diagnostico">
-         <h3>Diagnóstico y Prevención</h3>
-         <p>Una valoración integral y limpieza cada 6 meses ayuda a prevenir enfermedades en tu boca,  mantener tus encías y dientes sanos.</p>
-<p>Consiste en una sola cita donde también te orientamos para que lleves un buen cuidado de tu boca en casa.</p>
-         </div>
-         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Copia de 001-dental.png" alt="">
-         <h3>Implante Dental</h3>
-          <p>Son la mejor opción para recuperar la función y estética de uno o varios dientes perdidos e incluso la dentadura completa. Existen diferentes implantes con tamaños y formas variadas que se adecúan a cada caso en particular.
-</p>
-<p>Se requiere de una cita de diagnóstico previa para determinar la condición individual de cada paciente.</p></div>
-   
-    <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/004-dentist.png" alt="">
-         <h3>Odontología Restauradora
-</h3>
-          <p>Existen diferentes opciones para recuperar la forma, función y estética de un diente lastimado o perdido. 
-</p>
-<p>
-Desde opciones fijas y removibles como una resina o corona hasta una prótesis parcial o total removible (placa).
-</p>
-<p>
-Con nuestra asesoría podrás elegir la que mejor se adapte a tu problema y estilo de vida.</p></div>
     </div>
-    </div>
+</section>
+<section>
     <div id="servicios" class="template-section">
-         <div class="grid">
-         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/002-gum.png" alt="diagnostico">
-         <h3>Periodoncia</h3>
-         <p>En esta área nos enfocamos en la salud de tus encías y del tejido de soporte del diente (ligamento periodontal y hueso). 
-</p>
-<p>
-Ayudando a prevenir y controlar a la enfermedad periodontal.</p>
-         </div>
-         <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Copia de 003-teeth.png" alt="">
-         <h3>Atención Integral para niños</h3>
-          <p>Nos encargamos del diagnóstico, prevención, tratamiento y control de los problemas de salud bucal de los más pequeños de casa. </p></div>
-   
-    <div  class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/007-teeth-3.png" alt="">
-         <h3>Atención a
-Urgencias
+        <h2 class="valores_titulo">SERVICIOS ODONTOLÓGICOS EN CDMX</h2>
+        <h3>Contamos con atención personalizada cubriendo <br>
+            todas las áreas odontológicas</h3>
+        <div class="grid">
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/005-teeth-1.png"
+                    alt="diagnostico">
+                <h3>Diagnóstico y Prevención</h3>
+                <p>Una valoración integral y limpieza cada 6 meses ayuda a prevenir enfermedades en tu boca, mantener
+                    tus encías y dientes sanos.</p>
+                <p>Consiste en una sola cita donde también te orientamos para que lleves un buen cuidado de tu boca en
+                    casa.</p>
+            </div>
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Copia de 001-dental.png"
+                    alt="">
+                <h3>Implante Dental en CDMX</h3>
+                <p>Son la mejor opción para recuperar la función y estética de uno o varios dientes perdidos e incluso
+                    la dentadura completa. Existen diferentes implantes con tamaños y formas variadas que se adecúan a
+                    cada caso en particular.
+                </p>
+                <p>Se requiere de una cita de diagnóstico previa para determinar la condición individual de cada
+                    paciente.</p>
+            </div>
 
-</h3>
-          </div>
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/004-dentist.png" alt="">
+                <h3>Odontología Restauradora
+                </h3>
+                <p>Existen diferentes opciones para recuperar la forma, función y estética de un diente lastimado o
+                    perdido.
+                </p>
+                <p>
+                    Desde opciones fijas y removibles como una resina o corona hasta una prótesis parcial o total
+                    removible (placa).
+                </p>
+                <p>
+                    Con nuestra asesoría podrás elegir la que mejor se adapte a tu problema y estilo de vida.</p>
+            </div>
+        </div>
     </div>
+    <div id="servicios" class="template-section">
+        <div class="grid">
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/002-gum.png"
+                    alt="diagnostico">
+                <h3>Periodoncia</h3>
+                <p>¿Buscas Periodoncista?, En esta área nos enfocamos en la salud de tus encías y del tejido de soporte del diente (ligamento
+                    periodontal y hueso).
+                </p>
+                <p>
+                    Ayudando a prevenir y controlar a la enfermedad periodontal.</p>
+            </div>
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/Copia de 003-teeth.png"
+                    alt="">
+                <h3>Atención Integral para niños</h3>
+                <p>Nos encargamos del diagnóstico, prevención, tratamiento y control de los problemas de salud bucal de
+                    los más pequeños de casa. </p>
+            </div>
+
+            <div class="col2"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/007-teeth-3.png" alt="">
+                <h3>Atención a
+                    Urgencias
+
+                </h3>
+            </div>
+        </div>
     </div>
 </section>
 
 
-    <section id="doctora" class="template-section">
-        <div class="caracteristicas1">
+<section id="doctora" class="template-section">
+    <div class="caracteristicas1">
         <div class="contenedor">
-            
+
             <div class="grid">
-               
+
                 <div class="col">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/doctora.png" alt="Doctora">
                 </div>
                 <div class="col">
-                 <h2 class="tipo">Dra. Megan González Soto</h2>
-                  <p>Egresada de la Facultad de Odontología UNAM </p>
-                   <h3>Misión:</h3>
+                    <h2 class="tipo">Dra. Megan González Soto</h2>
+                    <p>Egresada de la Facultad de Odontología UNAM </p>
+                    <h3>Misión:</h3>
                     <p class="azul">
-                       
-                    Brindar un servicio dental integral personalizado y comprometido con el cuidado y prevención de la salud bucal para lograr la satisfacción y bienestar del paciente. 
+
+                        Brindar un servicio dental integral personalizado y comprometido con el cuidado y prevención de
+                        la salud bucal para lograr la satisfacción y bienestar del paciente.
                         <h3>Visión:</h3>
-                    <p class="azul">
-                       
-                    Fomentar la educación en cuanto a la prevención, mejorando así el nivel de salud bucal de cada uno de nuestros pacientes.
-            
-                        
-                    </p>
-                    <a href="https://dentistamegan.mx/#contacto" class="btn aqua">¡Contactar!</a>
+                        <p class="azul">
+
+                            Fomentar la educación en cuanto a la prevención, mejorando así el nivel de salud bucal de
+                            cada uno de nuestros pacientes.
+
+
+                        </p>
+                        <a href="https://dentistamegan.mx/#contacto" class="btn aqua">¡Contactar!</a>
                 </div>
             </div>
         </div>
-        </div>
+    </div>
 </section>
-    <section id="colaborador" class="template-section">
-        <div class="caracteristicas1">
+<section id="colaborador" class="template-section">
+    <div class="caracteristicas1">
         <div class="contenedor">
-            
+
             <div class="grid">
-               
+
                 <div class="col">
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/colaborador.png" alt="Doctora">
                 </div>
                 <div class="col">
-                 <h2>COLABORADORES</h2>
-                 
-                   <h3>Dr. Santiago H. Díaz Prieto</h3>
+                    <h2>COLABORADORES</h2>
+
+                    <h3>Dr. Santiago H. Díaz Prieto</h3>
                     <p>
-                       <ul>
-                           <li>
-                            Egresado de la Facultad de Odontología UNAM.
-                           </li>
-                           <li> Especialista en Periodoncia e Implantología en la
-    División de Estudios de Posgrado e Investigación. 
-    UNAM.</li>
-                       </ul>
+                        <ul>
+                            <li>
+                                Egresado de la Facultad de Odontología UNAM.
+                            </li>
+                            <li> Especialista en Periodoncia e Implantología en la
+                                División de Estudios de Posgrado e Investigación.
+                                UNAM.</li>
+                        </ul>
 
-   
-                
+
+
                 </div>
             </div>
         </div>
-        </div>
+    </div>
 </section>
- 
- <section id="galeria" class="template-section">
- <h2>GALERÍA</h2>
-  <?php echo do_shortcode( '[elfsight_instagram_feed id="1"]' ); ?>
- 
- </section>
 
- <section id="contacto" class="template-section padding-50">
+<section id="galeria" class="template-section">
+    <h2>GALERÍA</h2>
+    <?php echo do_shortcode( '[elfsight_instagram_feed id="1"]' ); ?>
 
-        <div class="contenedor">
+</section>
 
-            <div class="grid">
+<section id="contacto" class="template-section padding-50">
 
-                <div class="col prueba_col">
-                   <h2>¡Agenda tu cita!</h2>
-                    <?php echo do_shortcode( '[caldera_form id="CF5babff4a364a4"]' ); ?>
+    <div class="contenedor">
+
+        <div class="grid">
+
+            <div class="col prueba_col">
+                <h2>¡Agenda tu cita!</h2>
+                <?php echo do_shortcode( '[wpforms id="125"]' ); ?>
+            </div>
+
+            <div class="col">
+
+                <div class="datos">
+                    <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/001-whatsapp.png" alt="telefono"> (044)
+                    55 44 95 61 65
                 </div>
 
-                <div class="col">
 
-                    <div class="datos">
-                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/001-whatsapp.png" alt="telefono"> (044) 55 44 95 61 65
+                <div class="grid datos">
+                    <div class="info">
+                        Consultorio Privado
+                        Odontología Integral en CDMX
+                        <hr>
+                        Periférico sur 5264, <br>
+                        Col. Pedregal de Carrasco. <br>Coyoacán, C.P. 04700 Ciudad de México.
                     </div>
-
-
-                    <div class="grid datos">
-                        <div class="info">
-                            Periférico sur 5264, <br>
-Col. Pedregal de Carrasco. <br>Coyoacán, C.P. 04700 CDMX.
-                        </div>
-                    </div>
-
-
                 </div>
 
 
             </div>
+
+
         </div>
-    </section>
-    
-    
+    </div>
+</section>
+
+
 <?php 
     get_footer();
 ?>
